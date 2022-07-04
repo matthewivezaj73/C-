99af86b97078e5dd1dbe59d77cac7a890cb1c1c1
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-07-03 20:04:57 -0400

    Added a comment.

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿//Using system.
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
