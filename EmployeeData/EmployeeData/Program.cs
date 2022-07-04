63aa1baf02b7765d363d66862c9abfcd026a95ec
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-07-04 12:02:08 -0400

    Commit

--- EmployeeData/EmployeeData/Program.cs
+++ EmployeeData/EmployeeData/Program.cs
@@ -13,12 +13,16 @@ namespace Employee {
         //Adding private variables.
         private int employeeID;
         private string name;
+        private String StreetAddress;
+        private int zipCode;
         //Creating a main method.
         static void Main(String[] args)
         {
             //Qualifying the members of the class by using this.
             String name;
             int employeeID;
+            String StreetAddress;
+            int zipCode;
 
             //Printing out a message to the user.
             Console.WriteLine("Please enter the employee's name:   ");
@@ -29,6 +33,16 @@ namespace Employee {
             Console.WriteLine("Please enter the employee's ID:   ");
             //Creating a console object.
             employeeID = Convert.ToInt32(Console.ReadLine());
+
+            //Printing out a message to the user.
+            Console.WriteLine("Please enter the employee's street address:   ");
+            //Creating a console object.
+            StreetAddress = Console.ReadLine();
+
+            //Printing out a message to the user.
+            Console.WriteLine("Please enter the employee's zip code:   ");
+            //Creating a console object.
+            zipCode = Convert.ToInt32(Console.ReadLine());
         }
     }
 }
