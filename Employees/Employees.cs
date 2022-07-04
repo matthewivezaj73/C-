f9e6bc35e4950cebad3f2324889f038bd0fbeb3c
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-07-04 09:59:21 -0400

    Created a namespace.

--- Employees/Employees.cs
+++ Employees/Employees.cs
@@ -6,22 +6,24 @@
 //Importing system.
 using System;
 //Creating a namespace.
-//Creating a public class.
-class Employee
-{
-    //Adding private variables.
-    private string alias;
-    private string name;
-    //Creating a public constructor.
-    static void Main(String[] args)
+namespace Employee {
+    //Creating a public class.
+    class Employee
     {
-        //Qualifying the members of the class by using this.
-        this.name = name;
-        this.employeeID = employeeID;
-        //Creating a console object.
-        Console = console.ReadLine();
-        //Printing out a message to the user.
-        Console.WriteLine("Please enter the employee's name:   ");
-        //Reading the line from the user as input.
+        //Adding private variables.
+        private string alias;
+        private string name;
+        //Creating a public constructor.
+        static void Main(String[] args)
+        {
+            //Qualifying the members of the class by using this.
+            this.name = name;
+            this.employeeID = employeeID;
+            //Creating a console object.
+            Console = console.ReadLine();
+            //Printing out a message to the user.
+            Console.WriteLine("Please enter the employee's name:   ");
+            //Reading the line from the user as input.
+        }
     }
 }
