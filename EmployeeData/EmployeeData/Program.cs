8813491d6d33119fb255431ac7597f26951319ff
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-07-04 11:15:20 -0400

    Pasted code in script.

--- EmployeeData/EmployeeData/Program.cs
+++ EmployeeData/EmployeeData/Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+/*
+    Matthew Ivezaj
+    7/4/2022
+    Employees
+*/
+//Importing system.
+using System;
+//Creating a namespace.
+namespace Employee {
+    //Creating a public class.
+    class Employee
+    {
+        //Adding private variables.
+        private string alias;
+        private string name;
+        //Creating a public constructor.
+        static void Main(String[] args)
+        {
+            //Qualifying the members of the class by using this.
+            this.name = name;
+            this.employeeID = employeeID;
+            //Creating a console object.
+            Console = Console.ReadLine();
+            //Printing out a message to the user.
+            Console.WriteLine("Please enter the employee's name:   ");
+            //Reading the line from the user as input.
+        }
+    }
+}
