b355e3736936442208f5161ca8e11f4ef53c10bb
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-07-04 09:58:07 -0400

    Added parenthesis to readline.
    Changed string to String.

--- Employees/Employees.cs
+++ Employees/Employees.cs
@@ -12,13 +12,13 @@ public class Employee
     private string alias;
     private string name;
     //Creating a public constructor.
-    static void Main(string [] args)
+    static void Main(String[] args)
     {
         //Qualifying the members of the class by using this.
         this.name = name;
         this.employeeID = employeeID;
         //Creating a console object.
-        Console = console.ReadLine
+        Console = console.ReadLine();
         //Printing out a message to the user.
         Console.WriteLine("Please enter the employee's name:   ");
         //Reading the line from the user as input.
