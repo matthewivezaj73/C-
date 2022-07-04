26103a105a2b80c479fdd1daf14296ecd49d3037
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-07-03 22:08:09 -0400

    added enum Lanuage as a private

--- CSharp and Data Types/Demo.cs
+++ CSharp and Data Types/Demo.cs
@@ -1,17 +1,24 @@
-//Creating an integer.
-int number;
-//Assigning a number to the variable.
-number = 500;
-//Overloading the variable.
-int number = 500;
-//Creating a const value.
-const int DAYS_IN_WEEK = 7;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Creating an integer.
+        int number;
+        //Assigning a number to the variable.
+        number = 500;
+        //Overloading the variable.
+        int number = 500;
+        //Creating a const value.
+        const int DAYS_IN_WEEK = 7;
+        //Using defined enumerations as a data type.
+        Language lan = Language.PL;
+        switch (Language)
+        {
+            case Language.PL:/* Polish version */ break;
+            case Language.DE:/* German Version */ break;
+        }
+    }
+}
+
 //Creating an enumeration.
 enum Language {PL, EN, DE}
-//Using defined enumerations as a data type.
-Language lan = Language.PL;
-switch(Language)
-{
-    case Language.PL:/* Polish version */ break;
-    case Language.DE:/* German Version */ break;
-}
